8e421008f395eb33929cb2ee4dd5501f3543242d
王海涛 <15124505701@163.com>
2018-02-01 10:42:41 +0800

    fetch and lazyload

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,14 +1,57 @@
 (function() {
-  let slider = new Slider({
-    el: document.querySelector("#slider"),
-    slides: [
-      { link: "#1", image: "images/slider1.jpg" },
-      { link: "#2", image: "images/slider2.jpg" },
-      { link: "#3", image: "images/slider3.jpg" },
-      { link: "#4", image: "images/slider4.jpg" },
-      { link: "#5", image: "images/slider5.jpg" }
-    ]
-  });
+  fetch("/json/rec.json")
+    .then(res => res.json())
+    .then(render);
 
-  window.slider = slider;
+  function render(json) {
+    renderSlider(json.data.slider);
+    renderRadios(json.data.radioList);
+    renderPlaylists(json.data.songList);
+    lazyload(document.querySelectorAll(".lazyload"));
+  }
+
+  function renderSlider(slides) {
+    slides = slides.map(slide => {
+      return { link: slide.linkUrl, image: slide.picUrl };
+    });
+    new Slider({
+      el: document.querySelector("#slider"),
+      slides
+    });
+  }
+
+  function renderRadios(radios) {
+    document.querySelector(".radios .list").innerHTML = radios
+      .map(
+        radio =>
+          `<div class="list-item">
+          <div class="list-media">
+            <img class="lazyload" data-src="${radio.picUrl}">
+            <span class="icon icon-play"></span>
+          </div>
+          <div class="list-detail">
+            <h3 class="list-title">${radio.Ftitle}</h3>
+          </div>
+        </div>`
+      )
+      .join("");
+  }
+
+  function renderPlaylists(playlists) {
+    document.querySelector(".playlists .list").innerHTML = playlists
+      .map(
+        list =>
+          `<div class="list-item">
+          <div class="list-media">
+            <img class="lazyload" data-src="${list.picUrl}">
+            <span class="icon icon-play"></span>
+          </div>
+          <div class="list-detail">
+            <h3 class="list-title">${list.songListDesc}</h3>
+            <div class="list-text"></div>
+          </div>
+        </div>`
+      )
+      .join("");
+  }
 })();
